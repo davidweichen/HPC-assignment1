faf71fba3eaa8eb908355b3f74d960fcc8c2de68
vboxuser <vboxuser@HPC.myguest.virtualbox.org>
2023-08-29 22:34:02 -0700

    test

--- sum_direct.cpp
+++ sum_direct.cpp
@@ -11,14 +11,17 @@
 void 
 setup(int64_t N, uint64_t A[])
 {
-   printf(" inside direct_sum problem_setup, N=%lld \n", N);
+   printf(" inside sum_indirect problem_setup, N=%lld \n", N);
 }
-
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-   printf(" inside direct_sum perform_sum, N=%lld \n", N);
+   int64_t sum=0;
+   for(int i=0;i<N;i++){
+      sum+=i;
+   }
+   printf(" inside sum_indirect perform_sum, N=%lld \n", N);
 
-   return 0;
+   return sum;
 }
 
--- sum_vector.cpp
+++ sum_vector.cpp
@@ -11,14 +11,21 @@
 void 
 setup(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_vector problem_setup, N=%lld \n", N);
+   for(int i=0;i<N;i++){
+      std::cout << "Please enter element "<< i+1 << ": ";
+      std::cin >> A[i];
+   }
+   std::cout <<" inside direct_sum problem_setup, N = " << N << std::endl;
 }
-
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_vector perform_sum, N=%lld \n", N);
-
-   return 0;
+   int64_t sum=0;
+   for(int i=0;i<N;i++){
+      sum+=A[i];
+   }
+   std::cout << "sum is " << sum << std::endl;
+   std::cout <<" inside direct_sum perform_sum, N = " << N << std::endl;
+   return sum;
 }
 
