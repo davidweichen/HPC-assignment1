683517ef6b4cac11e6dfef38bed6596b3aa74e61
davidweichen <124118243+davidweichen@users.noreply.github.com>
2023-08-30 02:10:05 -0700

    Update sum_indirect.cpp

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -11,14 +11,30 @@
 void 
 setup(int64_t N, uint64_t A[])
 {
+   std::srand(std::time(nullptr));
+   for (int i = 0; i < N; ++i) {
+        A[i] = std::rand() % n;
+        std::cout << A[i] << std::endl;
+    }
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
+    int sum = 0;
+    int indx = 0;
+    while (indx < n) {
+        if(A[indx]>=0){
+            sum += A[indx];
+            A[indx] = -1;
+            indx = A[indx];
+        }else{
+            indx++;
+        }
+    }
    printf(" inside sum_indirect perform_sum, N=%lld \n", N);
 
-   return 0;
+   return sum;
 }
 
