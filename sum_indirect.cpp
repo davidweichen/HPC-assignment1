e7795657cd0398e68031a5aa345710f5f00357cc
davidweichen <david19990212@gmail.com>
2023-09-02 14:08:11 -0700

    overflow prob fix

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -5,15 +5,14 @@
 #include <random>
 #include <vector>
 #include <string.h>
-
+#include <stdlib.h>
 
 
 void 
 setup(int64_t N, uint64_t A[])
 {
-   std::srand(std::time(nullptr));
    for (int i = 0; i < N; ++i) {
-        A[i] = std::rand() % N;
+        A[i] = lrand48() % N;
     }
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
 }
@@ -21,7 +20,7 @@ setup(int64_t N, uint64_t A[])
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-    int sum = 0;
+    int64_t sum = 0;
     int indx = 0;
     int count = 0;
     while (count< N) {
