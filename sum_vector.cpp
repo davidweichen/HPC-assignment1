e14093684b7df000b6cf87db9380d22a19abf4a2
davidweichen <david19990212@gmail.com>
2023-09-04 00:21:56 -0700

    changed

--- sum_vector.cpp
+++ sum_vector.cpp
@@ -14,7 +14,7 @@ setup(int64_t N, uint64_t A[])
    for(int i=0;i<N;i++){
       A[i] = i;
    }
-   std::cout <<" inside direct_sum problem_setup, N = " << N << std::endl;
+   printf(" inside sum_vector problem_setup, N=%lld \n", N);
 }
 int64_t
 sum(int64_t N, uint64_t A[])
@@ -23,8 +23,7 @@ sum(int64_t N, uint64_t A[])
    for(int i=0;i<N;i++){
       sum+=A[i];
    }
-   std::cout << "sum is " << sum << std::endl;
-   std::cout <<" inside direct_sum perform_sum, N = " << N << std::endl;
+   printf(" inside sum_vector problem_setup, N=%lld \n", N);
    return sum;
 }
 
