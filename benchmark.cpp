8ffd1d4b9ebbca66a95c933d5a59dbf50f4070ac
davidweichen <david19990212@gmail.com>
2023-09-05 20:22:43 -0700

    .

--- benchmark.cpp
+++ benchmark.cpp
@@ -45,7 +45,9 @@ int main(int argc, char** argv)
       t = sum(n, &A[0]);
 
       // insert your end timer code here, and print out elapsed time for this problem size
-   
+      std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> elapsed = end_time - start_time;
+      std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
       printf(" Sum result = %lld \n",t);
 
    } // end loop over problem sizes
