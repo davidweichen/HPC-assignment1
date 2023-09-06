db9b548cc989d4b97f18604142c68ea29c95f5da
davidweichen <david19990212@gmail.com>
2023-09-05 19:29:32 -0700

    some change

--- plot_3vars.py
+++ plot_3vars.py
@@ -20,7 +20,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-fname = "sample_data_3vars.csv"
+fname = "cp1.csv"
 df = pd.read_csv(fname, comment="#")
 print(df)
 
--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -23,7 +23,6 @@ sum(int64_t N, uint64_t A[])
 {
     printf(" inside sum_indirect perform_sum, N=%lld \n", N);
     int64_t sum = 0;
-    int count = 0;
     int indx = 0;
     for(int i=0;i<N;i++){
       sum+=A[indx];
